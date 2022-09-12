93f64429f6c10dcdfbb1b32a86346e67e9e88638
=Gabriel-Quadros <=gquadros312@gmail.com>
2022-09-12 09:21:54 -0300

    12/09

--- livraria/urls.py
+++ livraria/urls.py
@@ -2,14 +2,26 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-from core.views import CategoriaViewSet, EditoraViewSet
+
+from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet
 
 router = DefaultRouter()
-router.register(r'categorias', CategoriaViewSet)
-router.register(r'editoras', EditoraViewSet)
+router.register(r"autores", AutorViewSet)
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"editoras", EditoraViewSet)
+router.register(r"livros", LivroViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    # Admin
+    path("admin/", admin.site.urls),
+    # SimpleJWT - Login
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # API DRF
+    path("", include(router.urls)),
 ]
